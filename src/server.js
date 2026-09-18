524eae5c942d2f20960ede027ea07cca38d469ee
nadjanilsonbarbosaferreira11-bip <nadjanilsonbarbosaferreira11@gmail.com>
2026-09-18 14:16:55 -0300

    atualizando api

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const express = require("express")
 const cors = require("cors")
 const db = require("./config/database")
 
+
 const app = express()
 
 const PORT = 3001
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/",(req,res)=>{
     res.json({
@@ -15,7 +17,18 @@ app.get("/",(req,res)=>{
     })
 })
 
-app.post("/produto", async (req, res) => {
+app.get("/produtos", async (req, res)=>{
+    try {
+        const [produtos] = await db.query(
+            "select * from produto"
+        )
+        res.json(produtos)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post("/produtos", async (req, res) => {
     try {
         const { descricao, categoria, preco, imagem } = req.body;
 
@@ -50,117 +63,20 @@ app.post("/produto", async (req, res) => {
         });
     }
 });
-
-app.listen(PORT, ()=>{
-    console.log("Servidor rodando na porta 3001")
-})
-
-const express = require("express")
-const cors = require("cors")
-const db = require("./config/database")
-
-const app = express()
-
-const PORT = 3001
-
-app.use(express.json())
-
-app.get("/",(req,res)=>{
-    res.json({
-        mensagem:"API funcionando"
-    })
-})
-
-app.post("/produto", async (req, res) => {
+app.delete("/produtos/:id", async(req, res)=>{
     try {
-        const { descricao, categoria, preco, imagem } = req.body;
+        const {id} = req.params
 
-        const sql = `
-            INSERT INTO produto (descricao, categoria, preco, imagem)
-            VALUES (?, ?, ?, ?)
-        `;
+        await db.query("delete from produto where id = ?", [id])
 
-        const [result] = await db.execute(sql, [
-            descricao,
-            categoria,
-            preco,
-            imagem
-        ]);
-
-        res.status(201).json({
-            mensagem: "Produto cadastrado com sucesso",
-            produto: {
-                id: result.insertId,
-                descricao,
-                categoria,
-                preco,
-                imagem
-            }
-        });
-
+        res.json({mensagem:"Produto deletado com sucesso!"})
     } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            mensagem: "Erro ao cadastrar produto"
-        });
+        console.log(error)
+        res.json({
+            erro: "Erro ao deletar o produto"
+        })
     }
-});
-
-app.listen(PORT, ()=>{
-    console.log("Servidor rodando na porta 3001")
 })
-const express = require("express")
-const cors = require("cors")
-const db = require("./config/database")
-
-const app = express()
-
-const PORT = 3001
-
-app.use(express.json())
-
-app.get("/",(req,res)=>{
-    res.json({
-        mensagem:"API funcionando"
-    })
-})
-
-app.post("/produto", async (req, res) => {
-    try {
-        const { descricao, categoria, preco, imagem } = req.body;
-
-        const sql = `
-            INSERT INTO produto (descricao, categoria, preco, imagem)
-            VALUES (?, ?, ?, ?)
-        `;
-
-        const [result] = await db.execute(sql, [
-            descricao,
-            categoria,
-            preco,
-            imagem
-        ]);
-
-        res.status(201).json({
-            mensagem: "Produto cadastrado com sucesso",
-            produto: {
-                id: result.insertId,
-                descricao,
-                categoria,
-                preco,
-                imagem
-            }
-        });
-
-    } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            mensagem: "Erro ao cadastrar produto"
-        });
-    }
-});
 
 app.listen(PORT, ()=>{
     console.log("Servidor rodando na porta 3001")
